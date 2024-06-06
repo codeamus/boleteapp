c1f481dc8c62c3b4b715f44bb00008e0d8343cb3
Alexander Urrutia <matias04041994@gmail.com>
2024-06-06 17:18:05 -0400

    avances

---
+++ src/utils/const.ts
@@ -0,0 +1,20 @@
+const IVA_VALUES = [
+  {
+    anio: 2021,
+    value: 11.5,
+  },
+  {
+    anio: 2022,
+    value: 12.25,
+  },
+  {
+    anio: 2023,
+    value: 13.0,
+  },
+  {
+    anio: 2024,
+    value: 13.75,
+  },
+];
+
+export { IVA_VALUES };
