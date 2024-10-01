2b2abc36c2cdda15e9fc3284f8b9b6a3f6de11a8
Alexander Urrutia <matias04041994@gmail.com>
2024-10-01 15:07:06 -0300

    Refactor Formulario.astro to update layout and input field behavior

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,19 +1,19 @@
 const IVA_VALUES = [
   {
-    anio: 2021,
-    value: 11.5,
-  },
-  {
-    anio: 2022,
-    value: 12.25,
+    anio: 2024,
+    value: 13.75,
   },
   {
     anio: 2023,
     value: 13.0,
   },
   {
-    anio: 2024,
-    value: 13.75,
+    anio: 2022,
+    value: 12.25,
+  },
+  {
+    anio: 2021,
+    value: 11.5,
   },
 ];
 
